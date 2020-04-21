569e5820fbd57a3f7a02cc6e9dc54ab4e9890cd4
Dowglas Maia <dowglasmaia@gmail.com>
2020-04-21 16:33:16 -0300

    first commit

---
+++ src/test/java/org/maia/livro/ApiLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maia.livro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
