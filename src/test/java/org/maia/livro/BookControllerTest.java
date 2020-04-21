b91f22f8f909f9409c9f3ee6d8f9a2f91ed215d5
Dowglas Maia <dowglasmaia@gmail.com>
2020-04-21 17:11:30 -0300

    Configurando BookControllerTests com Anotações JUnit5 e SpringTests

---
+++ src/test/java/org/maia/livro/BookControllerTest.java
@@ -0,0 +1,34 @@
+package org.maia.livro;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @DisplayName("Deve criar um livro com sucesso.")
+    public void createBookTest(){
+
+    }
+
+    @Test
+    @DisplayName("Deve lançar error de validação quando não houver dados suficientes para criação do livro.")
+    public void createInvalidBookTest(){
+
+    }
+
+}
