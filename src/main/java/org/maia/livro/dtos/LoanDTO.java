391c4df732d288e000f9cdc11b135317d7fb8db9
Dowglas Maia <dowglasmaia@gmail.com>
2020-06-28 21:27:12 -0300

    impl-> Teste de Integração - verificando se um livro emprestado não foi  devolvido

--- src/main/java/org/maia/livro/dtos/LoanDTO.java
+++ src/main/java/org/maia/livro/dtos/LoanDTO.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
 @Builder
 @AllArgsConstructor
