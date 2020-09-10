746d29796af7c338663ef0f59ce6f3c89dbe3c6a
dowglasmaia <dowglasmaia@gmail.com>
2020-09-10 12:01:39 -0300

    impl recurso de agendamento de terefas

--- src/main/java/org/maia/livro/ApiLivrosApplication.java
+++ src/main/java/org/maia/livro/ApiLivrosApplication.java
@@ -4,13 +4,24 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling // agendamento de tarefas
 public class ApiLivrosApplication {
 
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
+	}
+
+	/* * Criando Cronograma para agendamentos de tarefas
+	 * 	http://www.cronmaker.com/?1
+	 * */
+	@Scheduled(cron = "0 52 11 1/1 * ?") // segundo | minuto | hora | dia | mes | ano
+	public void testAgedamentoTarefas(){
+		System.out.println("Agendamento de tarefas com sucesso");
 	}
 
     public static void main(String[] args) {
