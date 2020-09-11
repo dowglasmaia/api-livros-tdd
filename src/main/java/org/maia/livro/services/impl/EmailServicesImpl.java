e6910d6ef72ff85b34c9fa384447f56097bfd65a
dowglasmaia <dowglasmaia@gmail.com>
2020-09-11 10:07:39 -0300

    impl metodo para envio de e-mail

---
+++ src/main/java/org/maia/livro/services/impl/EmailServicesImpl.java
@@ -0,0 +1,33 @@
+package org.maia.livro.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.maia.livro.services.interfaces.EmailServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EmailServicesImpl implements EmailServices {
+
+    @Value("${application.mail.default-remetente}")
+    private String remetent;
+
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    public void sendMails(String messagem, List<String> emailList) {
+
+        String [] mails = emailList.toArray( new String[ emailList.size() ] ); // convert a lista para um Array
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(remetent); //Remetente
+        mailMessage.setSubject("Livro com empréstimo atrasado"); // assunto
+        mailMessage.setText(messagem); // messagem do email
+        mailMessage.setTo(mails); // Array com Lista de email  - Destinatarios
+    }
+}
